008d7b27e085c9f17995c9b5c50782dda87c416e
7zog 🇩🇿 <188625578+7zog@users.noreply.github.com>
2025-04-06 13:51:42 +0200

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,18 +1,9 @@
 @echo off
-title 7zProject - Lancement
-echo Lancement de 7zProject...
-
-:: Vérifie si Python est installé
-python --version >nul 2>&1
-if %ERRORLEVEL% neq 0 (
-    echo Erreur : Python n'est pas installe ou n'est pas dans le PATH.
-    echo Veuillez installer Python depuis https://www.python.org/
-    pause
-    exit /b
-)
+title 7zog-Tools
+echo Lancement de 7zog-Tools...
 
 :: Change le répertoire et lance le fichier Python
-python 7z.py
+python 7zog.py
 
-:: Pause pour voir les erreurs éventuelles
-pause
+exit /b
+
